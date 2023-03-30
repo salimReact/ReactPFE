51c4743773da10cbcf1fbbd1fa1dd1a5153a08a8
salimReact <salim.mtiri@esen.tn>
2023-03-30 02:56:21 +0100

    first commit

--- src/pages/login/RegistrationChoice.jsx
+++ src/pages/login/RegistrationChoice.jsx
@@ -6,12 +6,12 @@ export default function RegistrationChoice() {
     <div className="container1">
    <div className="choicetitle text-center">Registration Choice</div>
    <div className="content">
-       <div className="user-details">
-           <a href="/EditorRegistrationPart1"><span className="choice">Editor</span></a>   
+       <div className="input-box">
+           <a href="/EditorRegistrationPart1"><span className="choice ">Editor</span></a>   
             </div>
             <hr></hr>
          <div className="input-box">
-           <a href="/AnnoncerRegistrationPart1"><span className="choice">Annoncer</span></a>   
+           <a href="/AnnoncerRegistrationPart1"><span className="choice ">Annoncer</span></a>   
             </div>
             </div>
    </div>
