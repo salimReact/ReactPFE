0f203e06a28995332eab5491f7a8d8719a2dc5c5
salimReact <salimmtiri17@gmail.com>
2023-04-14 20:06:01 +0100

    test

--- src/pages/ChangePassword.jsx
+++ src/pages/ChangePassword.jsx
@@ -1,7 +1,8 @@
-import React, { Fragment , useState , useEffect} from 'react'
-
+import React, { Fragment,useState,useEffect } from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import axios from 'axios';
 export default function ChangePassword() {
-
     const [passwordError, setPasswordError] = useState('');
     const [rnpasswordError, setRnpasswordError] = useState('');
     const [formData, setFormData] = useState({
@@ -9,95 +10,83 @@ export default function ChangePassword() {
         newPassword: "",
         repeatNewPassword :""
       });
-      
-      const handleChange = (event) => {
+    const handleChange = (event) => {
         setFormData({ ...formData, [event.target.name]: event.target.value });
       };
-      const [data, setData] = useState([]);
-      const userId = sessionStorage.getItem('userId');
-      console.log(userId );
-    useEffect(() => {
-      const fetchData = async () => {
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+        const requiredFields = ["password", "newPassword", "repeatnewPassword"]; // add other required fields here
+        const emptyFields = requiredFields.filter(
+          (field) => !formData[field].trim()
+        );
+        if (emptyFields.length > 0) {
+          alert(`Please fill in the following fields: ${emptyFields.join(", ")}`);
+          return;
+        }
         try {
-          const response = await axios.get(`http://localhost:3000/profile/${userId}`, { mode: 'cors' });
-          setData(response.data.data);
-          setFormData({
-            password: response.data.data.password,
+          const response = await axios.put(`http://localhost:3000/updateUser/${userId}`, formData, {
+            headers: { 'Content-Type': 'application/json' },
           });
+          setData(response.data.data);
+          console.log('User data updated successfully');
         } catch (error) {
           console.log(error);
         }
+        window.location.reload();
       };
-      fetchData();
-    }, [userId]);    
-
-    const handleSubmit = async (event) => {
-      
-      event.preventDefault();
-      const requiredFields = ["Fname", "username", "email"]; // add other required fields here
-      const emptyFields = requiredFields.filter(
-        (field) => !formData[field].trim()
-      );
-      if (emptyFields.length > 0) {
-        alert(`Please fill in the following fields: ${emptyFields.join(", ")}`);
-        return;
-      }
-      try {
-        const response = await axios.put(`http://localhost:3000/updateUser/${userId}`, formData, {
-          headers: { 'Content-Type': 'application/json' },
-        });
-        setData(response.data.data);
-        
-     
-        console.log('User data updated successfully');
-      } catch (error) {
-        console.log(error);
-      }
-      
-      window.location.reload();
-    
-    };
-    if (formData.password !== data.password) {
-        setPasswordError('Old password is incorrect');
-        return;
-      }
-    
-      if (formData.newPassword !== formData.repeatNewPassword) {
-        setRnpasswordError('New passwords do not match');
-        return;
-      } 
   return (
     <Fragment>
-         <hr className="my-4"/>
-                <h6 className="heading-small text-muted mb-4">Password</h6>
+        <Header/>
+        <div className="profile">
+
+        <div className="col-xl-8 order-xl-1">
+          <div className="card bg-secondary shadow">
+            <div className="card-header bg-white border-0">
+              <div className="row align-items-center">
+                <div className="col-8">
+                  <h3 className="mb-0">My account</h3>
+                </div>
+                <div className="col-4 text-right">
+                  <button onClick={handleSubmit} className='btn btn-sm btn-primary'>Update your password</button>
+                </div>
+              </div>
+            </div>
+            <div className="card-body">
+              <form>
+                <h6 className="heading-small text-muted mb-4">User information</h6>
                 <div className="pl-lg-4">
                   <div className="row">
-                    <div className="col-md-6">
-                      <div className="form-group focused">
-                        <label className="form-control-label" htmlFor="input-Password">Old Password</label>
-                        <input type="password"  id="input-Password" className="form-control form-control-alternative" placeholder="Previous password" onChange={handleChange} name='password'/>
-                      </div>
+                  <div className="col-md-6"> <div className="form-group focused"> <label className="form-control-label" htmlFor="input-Password">Old Password</label> <input type="password"  id="input-Password" className="form-control form-control-alternative" placeholder="Previous password" onChange={handleChange} name='password'/> 
+                    <div className="col-lg-6">
                     </div>
                   </div>
                   <div className="row">
-                    <div className="col-lg-6">
-                      <div className="form-group focused">
-                        <label className="form-control-label" htmlFor="input-NP">New Password</label>
-                        <input type="password" id="input-NP" className="form-control form-control-alternative" placeholder="New password"  onChange={handleChange} name='newPassword'/>
-                      </div>
-                    </div>
-                    <div className="col-lg-6">
-                      <div className="form-group focused">
-                        <label className="form-control-label" htmlFor="input-RNP">Repeat New Password</label>
-                        <input type="password" id="input-RNP" className="form-control form-control-alternative" placeholder="Repeat new password"  onChange={handleChange} name='repeatNewPassword'/>
-                      </div>
-                    </div>
                   </div>
-                  {passwordError && <div>{passwordError}</div>}
-            {rnpasswordError && <div>{rnpasswordError}</div>}
-            <button onClick={handleSubmit} className='btn btn-sm btn-primary'>Update your information</button>
-
                 </div>
+                 <div className="col-lg-6"> <div className="form-group focused"> 
+                 <label className="form-control-label" htmlFor="input-NP">New Password</label> 
+                 <input type="password" id="input-NP" className="form-control form-control-alternative" placeholder="New password"  onChange={handleChange} name='newPassword'/>
+                  </div> 
+                  </div> 
+                  <div className="col-lg-6"> <div className="form-group focused"> <label className="form-control-label" htmlFor="input-RNP">Repeat New Password</label>
+                   <input type="password" id="input-RNP" className="form-control form-control-alternative" placeholder="Repeat new password"  onChange={handleChange} name='repeatNewPassword'/> 
+                   </div>
+                    </div> 
+                    </div> 
+                    {passwordError && <div>{passwordError}</div>} {rnpasswordError && <div>{rnpasswordError}</div>} 
+                </div> 
+    
+              
+        
+
+              </form>
+            </div>
+            </div>
+            </div>
+            </div> 
+
+
+        <Footer/>
     </Fragment>
   )
 }
